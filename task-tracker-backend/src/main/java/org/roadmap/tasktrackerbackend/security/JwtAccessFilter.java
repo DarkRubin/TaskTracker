5fe6abe363f7b61123d6f2115a4be3ac1bb2b3a2
Vadim <botgugla@gmail.com>
2024-09-29 13:58:37 +0300

    feat: add JwtService and JwtAccessFilter for security

---
+++ task-tracker-backend/src/main/java/org/roadmap/tasktrackerbackend/security/JwtAccessFilter.java
@@ -0,0 +1,83 @@
+package org.roadmap.tasktrackerbackend.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
+import org.roadmap.tasktrackerbackend.service.JwtService;
+import org.roadmap.tasktrackerbackend.service.UserService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAccessFilter extends OncePerRequestFilter {
+
+    private static final String PREFIX = "Bearer ";
+
+    private final JwtService jwtService;
+
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(@NotNull HttpServletRequest request,
+                                    @NotNull HttpServletResponse response,
+                                    @NotNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        String token;
+        String header = request.getHeader("Authorization");
+        if (header == null || !header.startsWith(PREFIX)) {
+            try {
+                token = getTokenFromCookies(request);
+            } catch (NullPointerException e) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+        } else {
+            token = header.substring(PREFIX.length());
+        }
+        var username = jwtService.extractUserName(token);
+        if (Strings.isNotEmpty(username) && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userService.loadUserByUsername(username);
+            if (jwtService.isTokenValid(token, userDetails)) {
+                authorize(request, userDetails);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String getTokenFromCookies(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            throw new NullPointerException();
+        }
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("Access-Token")) {
+                return cookie.getValue();
+            }
+        }
+        throw new NullPointerException();
+    }
+
+    private void authorize(HttpServletRequest request, UserDetails userDetails) {
+        var authToken = new UsernamePasswordAuthenticationToken(
+                userDetails,
+                null,
+                userDetails.getAuthorities()
+        );
+        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        var context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(authToken);
+        SecurityContextHolder.setContext(context);
+    }
+}
---
+++ task-tracker-backend/src/main/java/org/roadmap/tasktrackerbackend/service/JwtService.java
@@ -0,0 +1,68 @@
+package org.roadmap.tasktrackerbackend.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.Setter;
+import org.roadmap.tasktrackerbackend.model.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+@Setter
+@Service
+public class JwtService {
+
+    @Value("${jwt.key}")
+    private String key;
+
+    private static final long TOKEN_EXPIRATION_TIME = 24 * 60 * 60 * 1000L;
+
+    public String generateToken(User user) {
+        Map<String, Object> claims = Map.of("id", user.getId(),
+                "email", user.getEmail(),
+                "password", user.getPassword());
+        return Jwts.builder().claims(claims)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + TOKEN_EXPIRATION_TIME))
+                .signWith(getSigningKey(), Jwts.SIG.HS256).compact();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String userName = extractUserName(token);
+        return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    public String extractUserName(String token) {
+        return extractAllClaims(token).get("email", String.class);
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolvers) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolvers.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().verifyWith(getSigningKey()).build()
+                .parseSignedClaims(token).getPayload();
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(key);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
