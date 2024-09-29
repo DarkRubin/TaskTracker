a0bafc24bf369504e9fa322684152744f60f0c64
Vadim <botgugla@gmail.com>
2024-09-29 13:54:31 +0300

    feat: add getByEmail method in repository

--- task-tracker-backend/src/main/java/org/roadmap/tasktrackerbackend/repository/UserRepository.java
+++ task-tracker-backend/src/main/java/org/roadmap/tasktrackerbackend/repository/UserRepository.java
@@ -11,5 +11,7 @@ public interface UserRepository extends CrudRepository<User, Integer> {
 
     Optional<User> findByEmailAndPassword(String email, String password);
 
+    User getByEmail(String email);
+
     Optional<User> findByEmail(String email);
 }
