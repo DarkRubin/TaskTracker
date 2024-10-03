8968dce02b022afc65df8bb3ff6e41623170a29f
Vadim <botgugla@gmail.com>
2024-10-03 18:57:58 +0300

    refactor: SecurityConfig remove unused injection and exception handling filter

--- task-tracker-backend/src/main/java/org/roadmap/tasktrackerbackend/security/SecurityConfig.java
+++ task-tracker-backend/src/main/java/org/roadmap/tasktrackerbackend/security/SecurityConfig.java
@@ -1,12 +1,11 @@
 package org.roadmap.tasktrackerbackend.security;
 
 import lombok.RequiredArgsConstructor;
-import org.roadmap.tasktrackerbackend.service.UserService;
+import org.roadmap.tasktrackerbackend.security.filter.ExceptionHandlerFilter;
+import org.roadmap.tasktrackerbackend.security.filter.JwtAccessFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,16 +19,15 @@ import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.List;
 
-import static org.springframework.http.HttpMethod.*;
+import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final UserService userService;
-
     private final JwtAccessFilter jwtAccessFilter;
+    private final ExceptionHandlerFilter exceptionHandlerFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -41,14 +39,14 @@ public class SecurityConfig {
                             List.of("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
                     configuration.setAllowCredentials(true);
                     return configuration;
-            }))
+                }))
                 .authorizeHttpRequests(configurator ->
-                configurator.requestMatchers(POST,"/user", "/auth/login").permitAll()
-                        .anyRequest().authenticated())
-                .authenticationProvider(authenticationProvider())
+                        configurator.requestMatchers(POST, "/user", "/auth/login").permitAll()
+                                .anyRequest().authenticated())
                 .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
-                .addFilterBefore(jwtAccessFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAccessFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(exceptionHandlerFilter, JwtAccessFilter.class);
         return http.build();
     }
 
@@ -56,14 +54,6 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
             throws Exception {
         return config.getAuthenticationManager();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userService);
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
     }
 
     @Bean
