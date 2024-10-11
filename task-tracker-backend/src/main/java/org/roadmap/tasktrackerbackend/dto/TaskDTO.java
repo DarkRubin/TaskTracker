404714abbacc90bd1910f59d75ded0d0a7820a76
Vadim <botgugla@gmail.com>
2024-10-11 18:35:43 +0300

    Add finishedTime field to TaskDTO
    This change introduces the finishedTime field to the TaskDTO class. It allows tracking of when a task was marked as completed.

--- task-tracker-backend/src/main/java/org/roadmap/tasktrackerbackend/dto/TaskDTO.java
+++ task-tracker-backend/src/main/java/org/roadmap/tasktrackerbackend/dto/TaskDTO.java
@@ -3,4 +3,4 @@ package org.roadmap.tasktrackerbackend.dto;
 import java.time.Instant;
 import java.util.UUID;
 
-public record TaskDTO(UUID uuid, String title, String text, Instant doBefore) {}
+public record TaskDTO(UUID uuid, String title, String text, Instant doBefore, Instant finishedTime) {}
