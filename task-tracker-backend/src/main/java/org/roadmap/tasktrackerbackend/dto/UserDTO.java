47a39e2de636ffc40f942a76df034aac92497e60
Vadim <botgugla@gmail.com>
2024-10-19 08:14:26 +0300

    refactor: remove NotNull annotation

--- task-tracker-backend/src/main/java/org/roadmap/tasktrackerbackend/dto/UserDTO.java
+++ task-tracker-backend/src/main/java/org/roadmap/tasktrackerbackend/dto/UserDTO.java
@@ -2,10 +2,9 @@ package org.roadmap.tasktrackerbackend.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record UserDTO(
-        @NotNull @NotEmpty @Email(regexp = "^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$") String email,
-        @NotNull @NotEmpty @Size(min = 8, max = 32) String password
+        @NotEmpty @Email(regexp = "^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$") String email,
+        @NotEmpty @Size(min = 8, max = 32) String password
 ) {}
