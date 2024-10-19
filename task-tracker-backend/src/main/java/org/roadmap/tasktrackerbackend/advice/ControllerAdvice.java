f63748015ca5eb363bc9b01b409dd3b6f865cecc
Vadim <botgugla@gmail.com>
2024-10-19 10:15:09 +0300

    refactor: add logger to ControllerAdvice

--- task-tracker-backend/src/main/java/org/roadmap/tasktrackerbackend/advice/ControllerAdvice.java
+++ task-tracker-backend/src/main/java/org/roadmap/tasktrackerbackend/advice/ControllerAdvice.java
@@ -1,5 +1,6 @@
 package org.roadmap.tasktrackerbackend.advice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.roadmap.tasktrackerbackend.exception.BasicRuntimeException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class ControllerAdvice {
 
@@ -16,7 +18,8 @@ public class ControllerAdvice {
     }
 
     @ExceptionHandler(Exception.class)
-    private ResponseEntity<Map<String, String>> sendErrorMessage() {
+    private ResponseEntity<Map<String, String>> sendErrorMessage(Exception ex) {
+        log.error(ex.getMessage(), ex);
         return ResponseEntity.status(500).body(Map.of("message", "Unexpected error"));
     }
 }
